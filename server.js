57590add2311bdf282dfc7ebb1334d4f9f48b5b4
Francisco Quintero <flquintero1@gmail.com>
2022-08-25 16:02:10 -0400

    added new server js

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
-const compression = require('compression');
 const express = require('express');
 const serveStatic = require('serve-static');
-const history = require('connect-history-api-fallback');
 const path = require('path');
-var app = express();
-function ensureSecure(req, res, next) {
-  //Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
-  if (req.get('X-Forwarded-Proto') == 'https' || req.hostname == 'localhost') {
-    //Serve Angular App by passing control to the next middleware
-    next();
-  } else if (req.get('X-Forwarded-Proto') != 'https' && req.get('X-Forwarded-Port') != '443') {
-    //Redirect if not HTTP with original request URL
-    res.redirect('https://' + req.hostname + req.url);
-  }
-}
-app.use(compression());
-app.use(ensureSecure);
 
-app.use(
-  history({
-    verbose: true,
-    disableDotRule: true,
-  })
-);
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 3000;
+const app = express();
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+const port = process.env.PORT || 8080;
 app.listen(port);
+console.log(`app is listening on port: ${port}`);

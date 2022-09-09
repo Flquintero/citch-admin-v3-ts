7d9d2448f503b5e04626b9a407f2ae8994d4e73e
Francisco Quintero <flquintero1@gmail.com>
2022-09-09 12:29:30 -0400

    added to csp

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(function (req, res, next) {
   res.setHeader(
     'Content-Security-Policy-Report-Only',
-    `default-src 'self'; script-src 'self' rocket-loader.min.js https://cdn.segment.com/analytics.js/v1//analytics.min.js https://www.google.com/recaptcha/api.js 'nonce-${nonce}';script-src-elem 'self' https://www.googletagmanager.com/gtag/js; style-src 'self'; style-src-elem 'self' addStylesClient.js 'nonce-${nonce}'; font-src 'self'; img-src 'self' https://files.citch.io; frame-src 'self' https://www.google.com; connect-src 'self'`
+    `default-src 'self'; script-src 'self' rocket-loader.min.js https://cdn.segment.com/analytics.js/v1//analytics.min.js https://www.google.com/recaptcha/api.js 'nonce-${nonce}';script-src-elem 'self' https://www.googletagmanager.com/gtag/js https://www.google.com/recaptcha/api.js https://www.gstatic.com/recaptcha/releases/g8G8cw32bNQPGUVoDvt680GA/recaptcha__en.js; style-src 'self'; style-src-elem 'self' addStylesClient.js 'nonce-${nonce}'; font-src 'self'; img-src 'self' https://files.citch.io; frame-src 'self' https://www.google.com; connect-src 'self'`
   );
 
   next();

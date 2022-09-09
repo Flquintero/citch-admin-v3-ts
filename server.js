19a8f07255d6d936a48813e90b4b8cd3f4263ddd
Francisco Quintero <flquintero1@gmail.com>
2022-09-09 10:20:07 -0400

    added csp changes

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
+const crypto = require('crypto');
+let nonce = crypto.randomBytes(16).toString('base64');
 
 const app = express();
 
 app.use(function (req, res, next) {
   res.setHeader(
     'Content-Security-Policy-Report-Only',
-    "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+    `default-src 'self'; script-src 'self' rocket-loader.min.js 'nonce-${nonce}' addStylesClient.js 'nonce-${nonce}' https://cdn.segment.com/analytics.js/v1//analytics.min.js https://www.google.com/recaptcha/api; style-src 'self'; style-src-elem 'self'; font-src 'self'; img-src 'self'; frame-src 'self'; connect-src 'self'`
   );
 
   next();

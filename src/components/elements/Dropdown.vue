1a3ae1339403e3308fa2eb8d2d665eaefc8ac845
Francisco Quintero <flquintero1@gmail.com>
2022-10-20 15:59:56 -0400

    added dropdown data and some shape

---
+++ src/components/elements/Dropdown.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <slot name="dropdown-trigger-title">
+        <span>{{ chosenOption || placeholder }}</span>
+      </slot>
+    </div>
+    <div>
+      <slot name="dropdown-list-content"> </slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Dropdown',
+  props: {
+    chosenOption: String,
+    placeholder: String,
+    options: Array as () => { value: string; text: string }[],
+  },
+});
+</script>
+<style lang="scss" scoped>
+.dropdown {
+}
+</style>
--- src/views/admin/platform-audience/platforms/facebook/FacebookAge.vue
+++ src/views/admin/platform-audience/platforms/facebook/FacebookAge.vue
@@ -1,14 +1,50 @@
 <template>
   <div class="facebook-audience-age">
-    <span>Age</span>
+    <div class="facebook-audience-age-min">
+      <CDropdown
+        @option-selected="setMinAge($event)"
+        v-bind="{
+          placeholder: 'Choose Min Age',
+          chosenOption: formData.ageMin,
+          options: formatAgeOptions(ageMinRange),
+        }" />
+    </div>
+    <div class="facebook-audience-age-max">
+      <CDropdown
+        @option-selected="setMaxAge($event)"
+        v-bind="{
+          placeholder: 'Choose Max Age',
+          chosenOption: formData.ageMax,
+          options: formatAgeOptions(ageMaxRange),
+        }" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { ageMinRange, ageMaxRange } from './utils/facebook-age-data';
+import CDropdown from '@/components/elements/Dropdown.vue';
 
 export default Vue.extend({
   name: 'FacebookAudienceAge',
+  components: { CDropdown },
+  data() {
+    return {
+      ageMinRange,
+      ageMaxRange,
+      formData: { ageMin: null as string | null, ageMax: null as string | null },
+    };
+  },
+  methods: {
+    setMinAge(age: string) {},
+    setMaxAge(age: string) {},
+    formatAgeOptions(ageRange: string[]) {
+      return ageRange.map((age: string) => {
+        return { value: age, text: age };
+      });
+    },
+  },
 });
 </script>
 <style lang="scss" scoped>

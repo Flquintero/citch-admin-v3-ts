1d4b053db9a6ee92ae886775467b55a4356ecb02
Francisco Quintero <flquintero1@gmail.com>
2022-11-07 14:24:44 -0500

    cleaned up code

--- src/utils/form-functionality.ts
+++ src/utils/form-functionality.ts
@@ -1,13 +1,11 @@
-import { IFormData, IValidationObject } from '@/types/forms';
+import { IFormData, IValidationObject } from '@/types/forms/interfaces';
 export const FormFunctions = {
   setFormValue(formData: IFormData, $v: IValidationObject, valueObject: { [k: string]: string }) {
     (formData as any)[valueObject.field] = valueObject.value;
     ($v as IValidationObject).formData[valueObject.field].$touch();
   },
   hasInputError($v: IValidationObject, field: string): () => boolean {
-    let error =
-      ($v as IValidationObject).formData[field].$error ||
-      ($v as IValidationObject).formData[field].$anyError;
+    let error = ($v as IValidationObject).formData[field].$error || ($v as IValidationObject).formData[field].$anyError;
     return error;
   },
   formatFormData(formData: IFormData) {
